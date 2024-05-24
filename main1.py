d7075f53a89fe11ee7bdeaf1f02443d29478c709
Aidar307 <163765369+Aidar307@users.noreply.github.com>
2024-05-24 12:50:08 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,8 @@
+import main2
+
+print("hello world")
+
+main2.say_hi()
+main2.so_hi()
+
+
---
+++ main2.py
@@ -0,0 +1,8 @@
+def say_hi():
+    print('hello world 2')
+
+a=25
+b=30
+
+def so_hi():
+    print('hello world 3')
